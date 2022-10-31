ebd27a5031eb0445c7e5d96bccfcf04670d512f9
bas <bastien.billey@gmail.com>
2022-10-31 12:02:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# NextJs Phaser Starter Template
+
+### Clone this repository
+
+    git clone git@github.com:isrmicha/nextjs-phaser.git
+
+### Install dependencys
+
+    yarn
+
+
+### Start development server
+
+    yarn dev
---
+++ src/components/game.tsx
@@ -0,0 +1,32 @@
+import { useRef } from "react";
+import { useGame } from "../helpers/useGame";
+import { Types } from "phaser";
+import { MainScene } from "./mainScene";
+
+const gameConfig: Types.Core.GameConfig = {
+  width: "100%",
+  height: "100%",
+  type: Phaser.AUTO,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  render: {
+    antialias: false,
+  },
+  scene: MainScene,
+};
+
+// you can place this in a different file if you prefer
+const PhaserGame = () => {
+  const parentEl = useRef<HTMLDivElement>(null);
+  useGame(gameConfig, parentEl);
+
+  return (
+    <div className="container">
+      <div ref={parentEl} />
+    </div>
+  );
+};
+
+export default PhaserGame;
