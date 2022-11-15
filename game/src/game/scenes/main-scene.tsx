75bb4f737b9cc5eb52e90617f446a7f73465a748
bas <bastien.billey@gmail.com>
2022-11-15 16:14:51 +0100

    test

--- game/src/game/scenes/main-scene.tsx
+++ game/src/game/scenes/main-scene.tsx
@@ -22,7 +22,7 @@ export class MainScene extends Phaser.Scene {
   private towerManager?: TowerManger;
   private bulletManager?: BulletManager;
 
-  client = new Client("wss://updeon.colyseus.de:2567");
+  client = new Client("wss://updeon.colyseus.de");
   room!: Room<GameState>;
 
   constructor() {
