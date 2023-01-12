83626cb17f8e6639602bd6d2df62c1de54de8fef
bas <bastien.billey@gmail.com>
2023-01-12 16:35:48 +0800

    fix build

--- game/src/game/entities/enemy/Enemy.ts
+++ game/src/game/entities/enemy/Enemy.ts
@@ -1,19 +1,16 @@
-import { kebabCase } from "lodash";
 import { Scene } from "phaser";
-import { cellSize } from "../../scenes/main-scene";
 
 export abstract class Enemy extends Phaser.GameObjects.Image {
+  constructor(scene: Scene, x: number, y: number, key: string) {
+    super(scene, x, y, key);
 
-    constructor(scene: Scene, x: number, y: number, key: string) {
-        super(scene, x, y, key);
+    scene.add.existing(this);
+  }
 
-        scene.add.existing(this);
-    }
+  update() {
+    const { serverX, serverY } = this.data.values;
 
-    update(){
-        const { serverX, serverY } = this.data.values;
-
-       this.x = Phaser.Math.Linear(this.x, serverX, 0.2);
-       this.y = Phaser.Math.Linear(this.y, serverY, 0.2);
-    }
+    this.x = Phaser.Math.Linear(this.x, serverX, 0.2);
+    this.y = Phaser.Math.Linear(this.y, serverY, 0.2);
+  }
 }
